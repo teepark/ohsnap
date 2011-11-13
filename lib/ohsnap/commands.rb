f17f47bbc4a48a18c6d96b5bab26d13290dd206e
Travis Parker <travis@claudia-thinkpad.(none)>
2011-11-12 22:36:13 -0800

    search command, and make the commands callable on the module

--- lib/ohsnap/commands.rb
+++ lib/ohsnap/commands.rb
@@ -1,11 +1,22 @@
-require 'rubygems'
-require 'sqlite3'
+require 'optparse'
+begin
+  require 'sqlite3'
+rescue LoadError
+  require 'rubygems'
+  require 'sqlite3'
+end
 
-require 'search'
+begin
+  require 'ohsnap'
+  require 'ohsnap/search'
+rescue LoadError
+#TODO: knock out this conditional require once we're in business as a gem
+  require 'search'
+end
 
 module OhSnap
   module Commands
-    def init(args, opts)
+    def self.init(args, opts)
       if File.exist?("meta.db")
         STDERR.write("OhSnap! meta.db already exists!")
         exit 78
@@ -27,14 +38,41 @@ module OhSnap
       Dir.mkdir("retouched")
     end
 
-    def tags(args, opts)
-      SQLite3::Database.new("meta.db") do |db|
+    def self.tags(args, opts)
+      SQLite3::Database.open("meta.db") do |db|
         db.execute("SELECT name FROM tag ORDER BY name") { |name| puts name }
-      end.close
+      end
     end
 
-    def search(args, opts)
-      
+    def self.search(args, opts)
+      specs = []
+      parser = OptionParser.new do |opts|
+        opts.on("-t", "--tags", "=SEARCH_SPEC",
+                "search for photos by their tags") do |spec|
+          specs << [OhSnap::Search::TAG, spec]
+        end
+
+        opts.on("-r", "--retouched",
+                "photos which have been retouched") do |spec|
+          specs << [OhSnap::Search::ORIGINAL, "&0"]
+        end
+
+        opts.on("-o", "--original",
+                "photos for which we still have the original") do |spec|
+          specs << [OhSnap::Search::ORIGINAL, "&1"]
+        end
+
+        opts.on("-e", "--extension", "=SEARCH_SPEC",
+                "search for photos by their filetype") do |spec|
+          specs << [OhSnap::Search::TYPE, spec]
+        end
+      end
+
+      parser.parse(args)
+
+      SQLite3::Database.open("meta.db") do |db|
+        OhSnap::Search.run(db, specs)
+      end
     end
   end
 end
